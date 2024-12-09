285dd09d893b8fe89b25f9c3680a328820735dcc
dheagaluhhh <dheagaluhamanda@gmail.com>
2024-12-09 15:07:20 +0700

    Normalize line endings to LF

---
+++ resources/about.blade.php
@@ -0,0 +1,8 @@
+@extends('layout')
+
+@section('title', 'About')
+
+@section('content')
+    <h1>About Us</h1>
+    <p>We are a Laravel-based application providing amazing solutions to our users!</p>
+@endsection
---
+++ resources/home.blade.php
@@ -0,0 +1,10 @@
+@extends('layout')
+
+@section('title', 'Home')
+
+@section('content')
+    <div class="text-center">
+        <h1>Welcome to Pawtopia PetShop</h1>
+        <p class="lead">This is the home page of your application.</p>
+    </div>
+@endsection
